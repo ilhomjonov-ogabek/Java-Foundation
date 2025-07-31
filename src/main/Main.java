3ab994bbdf69c1499eb15bc300b68e8c6407ef22
Ogabek <ilhomjonovo200@gmail.com>
2025-07-31 20:22:57 +0500

    lesson change

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,24 @@
 package main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c, d;
+        System.out.print("1-sonni kiriting:");
+        c = sc.nextInt();
+        System.out.print("2-sonni kiriting:");
+        d = sc.nextInt();
+        System.out.print("Natija:");
+        System.out.println(findMax(c,d));
+    }
 
+    public static int findMax(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
         }
-
+    }
 }
