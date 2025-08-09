b4f59471f3e054d6367012101263d874ec7e36e2
Ogabek <ilhomjonovo200@gmail.com>
2025-08-09 23:01:45 +0500

    lesson change

---
+++ src/main/Main9.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Main9 {
+    public static void main(String[] args) {
+        String str = "Salom";
+        System.out.println(str.substring(0));
+        System.out.println(str);
+
+    }
+}
