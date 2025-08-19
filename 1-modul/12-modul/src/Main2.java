ab0add35828c57ea051f8bbd42f50a99e1c99a1e
Ogabek <ilhomjonovo200@gmail.com>
2025-08-19 11:22:06 +0500

    lesson change

---
+++ 1-modul/12-modul/src/Main2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main2 {
+    static int sumOfDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return (n % 10) + sumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = sumOfDigits(number);
+        System.out.println("Raqamlari yig'indisi:" + sum);
+    }
+}
